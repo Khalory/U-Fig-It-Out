59727876f8ad5ea09d2bc6ab94efb21d52447c47
CraigFan96 <fancraig96@gmail.com>
2017-03-26 20:03:24 -0400

    Adding javascript for main page

--- app/app.js
+++ app/app.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Each major browser view user interface must be imported.
-import UI01 from './components/ui-01.js';
-import UI02 from './components/ui-02.js';
-
+import Main from './components/main.js'
 // For each view conditionally determine which view to display
 // depending on if the ID is present in the HTML.
 if (document.getElementById('ui-01') !== null) {
@@ -16,5 +14,11 @@ if (document.getElementById('ui-01') !== null) {
   ReactDOM.render(
     <UI02 />,
     document.getElementById('ui-02')
+  );
+} else if (document.getElementById('ui-03') !== null) {
+  console.log("yoyo")
+  ReactDOM.render(
+    <Main />,
+    document.getElementById('ui-03')
   );
 }
