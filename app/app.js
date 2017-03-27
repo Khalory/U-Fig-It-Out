651061ef35064803907ee9bfbc5178fce8ed9dfb
greg-robinson <greg.robinson85@gmail.com>
2017-03-26 21:23:05 -0400

    created searchlistings.js and item.js react components

--- app/app.js
+++ app/app.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Search from './components/search.js';
 
-// Each major browser view user interface must be imported.
-import UI01 from './components/ui-01.js';
-import UI02LIST from './components/ui-02-list.js';
-import UI02 from './components/ui-02.js';
-
-// For each view conditionally determine which view to display
-// depending on if the ID is present in the HTML.
-if (document.getElementById('ui-01') !== null) {
-  ReactDOM.render(
-    <UI01 />,
-    document.getElementById('ui-01')
-  );
-} else if (document.getElementById('ui-02-list') !== null) {
-  ReactDOM.render(
-    <UI02LIST />,
-    document.getElementById('ui-02-list')
-  );
-} else if (document.getElementById('ui-02') !== null) {
-  ReactDOM.render(
-    <UI02 />,
-    document.getElementById('ui-02')
-  );
-}
+ReactDOM.render(
+  <Search />,
+  document.getElementById('search')
+);
