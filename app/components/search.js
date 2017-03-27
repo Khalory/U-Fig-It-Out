64508eaefc8f1dbf9c9f879b6cbdbd4543cb60f7
greg-robinson <greg.robinson85@gmail.com>
2017-03-27 14:31:17 -0400

    modified search.js to use Navbar component

--- app/components/search.js
+++ app/components/search.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchLeftBar from './searchleftbar';
 import SearchListings from './searchlistings';
+import Navbar from './navbar'
 
 export default class Search extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         <SearchLeftBar />
         <SearchListings />
       </div>
