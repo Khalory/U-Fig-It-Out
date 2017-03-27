28a37bf0c946fc15478bf9af90d9af1145ed48c9
Khalory <tk100794@gmail.com>
2017-03-26 20:59:19 -0400

    Minor stuff

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // Each major browser view user interface must be imported.
 import newlisting from './components/newlisting'
@@ -11,16 +11,20 @@ if (document.getElementById('ui-01') !== null) {
   ReactDOM.render(
     <UI01 />,
     document.getElementById('ui-01')
-  );
+  )
 } else if (document.getElementById('ui-02') !== null) {
   ReactDOM.render(
     <UI02 />,
     document.getElementById('ui-02')
-  );
+  )
 } else if (document.getElementById('ui-03') !== null) {
-  console.log("yoyo")
   ReactDOM.render(
     <Main />,
     document.getElementById('ui-03')
   );
+} else if (document.getElementById('ui-03') !== null) {
+  ReactDOM.render(
+    <Main />,
+    document.getElementById('ui-03')
+  )
 }
