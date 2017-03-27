4bf06278e8727abddb52f8f32afcc267a66fce51
Khalory <tk100794@gmail.com>
2017-03-26 20:59:19 -0400

    Added basic React for the item listing page, including the acceptablePayments component

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // Each major browser view user interface must be imported.
 //import UI01 from './components/ui-01.js';
 import UI02 from './components/ui-02.js';
-import listingPage from './components/listingPage.js';
+import ListingPage from './components/listingPage.js';
 
 // For each view conditionally determine which view to display
 // depending on if the ID is present in the HTML.
@@ -20,7 +20,7 @@ if (document.getElementById('ui-01') !== null) {
   );
 } else if (document.getElementById('listingPage') !== null) {
   ReactDOM.render(
-    <listingPage />,
+    <ListingPage />,
     document.getElementById('listingPage')
   );
 }
