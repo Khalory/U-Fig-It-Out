7d9dd63904375f3990f7e49418366e1b62b96786
greg-robinson <greg.robinson85@gmail.com>
2017-04-08 16:19:28 -0400

    changed categories on searchleftbar.js

--- app/components/searchleftbar.js
+++ app/components/searchleftbar.js
@@ -1,76 +1,74 @@
 import React from 'react';
 
 export default class SearchLeftBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeIndex: null
+    }
+  }
+
+  handleClick(index) {
+    this.setState({activeIndex: index})
+  }
+
+
   render() {
-    return (
-      <div>
-        <div className="col-md-2 fig-categories">
-          <ul className="nav nav-pills nav-stacked">
-            <li role="presentation"><b>BOOKS</b></li>
-            <li role="presentation" className="active">
-              <a href="#">Textbooks</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Non-textbooks</a>
-            </li>
-            <li role="presentation"><b>CARS</b></li>
-            <li role="presentation">
-              <a href="#">Vehicles</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Automotive Accessories</a>
-            </li>
-            <li role="presentation"><b>CLOTHING</b></li>
-            <li role="presentation">
-              <a href="#">{`Men's`}</a>
-            </li>
-            <li role="presentation">
-              <a href="#">{`Women's`}</a>
-            </li>
-            <li role="presentation"><b>HOUSEHOLD</b></li>
-            <li role="presentation">
-              <a href="#">Furniture</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Refrigerators and Appliances</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Other Items</a>
-            </li>
-            <li role="presentation"><b>FIGS</b></li>
-            <li role="presentation"><b>ELECTRONICS</b></li>
-            <li role="presentation">
-              <a href="#">iClickers</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Cell Phones</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Other Electronic Items</a>
-            </li>
-            <li role="presentation"><b>COMPUTER</b></li>
-            <li role="presentation">
-              <a href="#">Laptops</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Desktops</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Computing Accessories</a>
-            </li>
-            <li role="presentation"><b>VIDEO GAMES</b></li>
-            <li role="presentation">
-              <a href="#">Consoles</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Games</a>
-            </li>
-            <li role="presentation">
-              <a href="#">Gaming Accessories</a>
-            </li>
-          </ul>
-        </div>
+    var categories = [
+        { name: <b>BOOKS</b> },
+        { name: "Textbooks" },
+        { name: "Non-textbooks" },
+        { name: <b>CARS</b>},
+        { name: "Vehicles"},
+        { name: "Automotive Accessories"},
+        { name: <b>CLOTHING</b>},
+        { name: "Men"},
+        { name: "Women"},
+        { name: <b>HOUSEHOLD</b>},
+        { name: "Furniture"},
+        { name: "Refrigerators and Appliances"},
+        { name: "Other Items"},
+        { name: <b>FIGS</b>},
+        { name: <b>ELECTRONICS</b>},
+        { name: "iClickers"},
+        { name: "Cell Phones"},
+        { name: "Other Electronic Items"},
+        { name: <b>COMPUTER</b>},
+        { name: "Laptops"},
+        { name: "Desktops"},
+        { name: "Computing Accessories"},
+        { name: <b>VIDEO GAMES</b>},
+        { name: "Consoles"},
+        { name: "Games"},
+        { name: "Gaming Accessories"}
+    ]
+
+    return <div>
+      <div className="col-md-2 fig-categories">
+        <ul className="nav nav-pills nav-stacked">
+        { categories.map((category, i) => {
+            return <Category key={i}
+              name={ category.name }
+              index={ i }
+              isActive={ this.state.activeIndex === i }
+              onClick={ this.handleClick.bind(this) }
+            /> } )
+        }
+        </ul>
       </div>
-    )
+    </div>
+  }
+}
+
+class Category extends React.Component {
+
+  handleClick() {
+    this.props.onClick(this.props.index)
+  }
+
+  render () {
+    return <li role="presentation" className={this.props.isActive ? "active" : "inactive"} onClick={this.handleClick.bind(this)}>
+      <a href="#">{this.props.name}</a>
+    </li>
   }
 }
