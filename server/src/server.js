2b9608723fd12a223fb279fc8d491c7726b19a2d
josephlew <joseph.j.lew@gmail.com>
2017-04-26 18:51:46 -0400

    Server now functional, sort of

--- server/src/server.js
+++ server/src/server.js
@@ -159,3 +159,21 @@ app.get('examplePath', function(req, res) {
 /*
 Start with app.POST/GET(ETC)
 */
+
+/**
+ * Translate JSON Schema Validation failures into error 400s.
+ */
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
+  }
+});
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
